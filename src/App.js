76196610e5b5f2f03775e06ece2824ead47f5742
bejiVIII <simone0443@outlook.com>
2024-04-20 19:57:08 +0300

    add search bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import ImageCard from "./components/imageCard.js";
+import ImageCard from "./components/ImageCard.js";
+import ImageSearch from "./components/ImageSearch.js";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -20,11 +21,16 @@ function App() {
 
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-3 gap-4">
-        {images.map((image) => (
-          <ImageCard key={image.id} image={image} />
-        ))}
-      </div>
+      <ImageSearch></ImageSearch>
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map((image) => (
+            <ImageCard key={image.id} image={image} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/*js",
-  ],
+  content: ["./src/*js", "./src/components/*js"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
