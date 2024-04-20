739ab1e3c94047f3000c7d0d7ae454f2ee7b9e67
bejiVIII <simone0443@outlook.com>
2024-04-20 23:42:24 +0300

    project cleanup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [term, setTerm] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(
       `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`,
     )
@@ -17,17 +18,19 @@ function App() {
         setIsLoading(false);
       })
       .catch((err) => console.log(err));
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-      <ImageSearch></ImageSearch>
+      <ImageSearch searchText={(text) => setTerm(text)} />
       {isLoading ? (
         <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1>
       ) : (
-        <div className="grid grid-cols-3 gap-4">
-          {images.map((image) => (
-            <ImageCard key={image.id} image={image} />
+        <div className="grid grid-cols-3 gap-3">
+          {images.map((image, index) => (
+            <div>
+              <ImageCard key={index} image={image} />
+            </div>
           ))}
         </div>
       )}
